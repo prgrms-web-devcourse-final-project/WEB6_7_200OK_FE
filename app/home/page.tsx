75add3da9df40a6221308d508bbe9d51b7ba3aba
elecharu <elec_haru@naver.com>
2025-12-09 15:47:31 +0900

    chore(structure): 프로젝트 폴더 구조 세팅

---
+++ app/home/page.tsx
@@ -0,0 +1,3 @@
+import { HomePage } from "@/layout";
+
+export default HomePage;
---
+++ src/layout/ui/home-page.tsx
@@ -0,0 +1,7 @@
+export function HomePage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">HOME PAGE</h1>
+    </div>
+  );
+}
