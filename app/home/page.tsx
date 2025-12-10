5de3c39d5fb5692051b0ffa91aacddf994348aaa
elecharu <elec_haru@naver.com>
2025-12-10 09:41:45 +0900

    fix(structure): 폴더구조 변경

--- app/home/page.tsx
+++
@@ -1,3 +0,0 @@
-import { HomePage } from "@/layout";
-
-export default HomePage;
--- src/screens/home/ui/HomeScreen.tsx
+++ src/screens/home/ui/HomeScreen.tsx
@@ -1,4 +1,4 @@
-export function HomePage() {
+export function HomeScreen() {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <h1 className="text-4xl font-bold">HOME PAGE</h1>
