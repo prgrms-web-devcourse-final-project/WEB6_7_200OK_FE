982deaf5a89281261ba547c2118f87afff3a223c
SeHyun <rla6170@naver.com>
2025-12-10 15:33:02 +0900

    chore(ui): change focus prefix visible -> within

--- src/shared/ui/input/file-input.tsx
+++ src/shared/ui/input/file-input.tsx
@@ -19,7 +19,7 @@ export default function FileInput({
       className={cn(
         "flex flex-col items-center justify-center",
         "selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input h-32 w-full min-w-0 rounded-md border bg-transparent text-base shadow-xs transition-[color,box-shadow] outline-none disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-base",
-        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+        "focus-within:border-ring focus-within:ring-ring/50 focus-within:ring-[3px]",
         "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
         className
       )}
