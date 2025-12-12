e960ed3ecf918adc5f5db11e584c7c510f0adb62
SeHyun <rla6170@naver.com>
2025-12-12 15:58:32 +0900

    feat(ui): add ImageCarousel component using next/image and shadcn carousel

---
+++ src/features/auction-detail/ui/image-carousel.tsx
@@ -0,0 +1,43 @@
+import type { StaticImport } from "next/dist/shared/lib/get-img-props";
+import Image from "next/image";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/shared/ui/carousel/carousel";
+
+interface ImageType {
+  src: string | StaticImport;
+  id: string;
+}
+
+interface ImageCarouselProps {
+  images: ImageType[];
+}
+
+export function ImageCarousel({ images }: ImageCarouselProps) {
+  return (
+    <div className="flex max-w-full">
+      <Carousel className="h-162 w-full shrink">
+        <CarouselContent className="h-162">
+          {images.map((v, _) => (
+            <CarouselItem key={v.id}>
+              <Image
+                src={v.src}
+                alt="image-carousel"
+                width="648"
+                height="648"
+                className="h-full w-full rounded-2xl object-cover dark:brightness-[0.2] dark:grayscale"
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="left-4" />
+        <CarouselNext className="right-4" />
+      </Carousel>
+    </div>
+  );
+}
