a94b03a95ab44e2d7d64f5a134f71b3033a0991f
SeHyun <31209436+sehyun0518@users.noreply.github.com>
2025-12-15 14:18:56 +0900

    chore(ui): product info text 태그 p태그로 변경

--- src/widgets/auction-detail/ui/product-info.tsx
+++ src/widgets/auction-detail/ui/product-info.tsx
@@ -6,8 +6,8 @@ export default function ProductInfo({ text }: ProductInfoProps) {
   const normalizedText = text.replace(/\\n/g, "\n");
   return (
     <div className="flex flex-col gap-2">
-      <span className="font-lg text-foreground font-semibold">상품 정보</span>
-      <span className="font-base text-foreground whitespace-pre-line">{normalizedText}</span>
+      <p className="font-lg text-foreground font-semibold">상품 정보</p>
+      <p className="font-base text-foreground whitespace-pre-line">{normalizedText}</p>
     </div>
   );
 }
