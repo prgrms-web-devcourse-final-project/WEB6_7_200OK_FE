56c7b71d81f322c76941d1e0d2b68722bd56b50d
SeHyun <rla6170@naver.com>
2025-12-15 15:16:35 +0900

    fix(auction-detail): 깃 추적 삭제된 파일 추가

---
+++ src/widgets/auction-detail/ui/image-carousel.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/shared/ui/carousel/carousel";
+
+interface ImageType {
+  src: string;
+  id: string;
+}
+
+interface ImageCarouselProps {
+  images: ImageType[];
+}
+
+export function ImageCarousel({ images }: ImageCarouselProps) {
+  return (
+    <div className="flex max-w-full">
+      <Carousel className="h-162 w-full shrink">
+        <CarouselContent className="h-162">
+          {images.map((image) => (
+            <CarouselItem key={image.id}>
+              <Image
+                src={image.src}
+                alt="auction image-carousel"
+                width={648}
+                height={648}
+                className="h-full w-full rounded-2xl object-cover dark:brightness-[0.2] dark:grayscale"
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="left-4" />
+        <CarouselNext className="right-4" />
+      </Carousel>
+    </div>
+  );
+}
