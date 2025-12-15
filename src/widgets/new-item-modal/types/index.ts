d2ddcea5d3e47784a3eebad29d4476565fc86503
elecharu <elec_haru@naver.com>
2025-12-15 18:16:09 +0900

    fix(date-utils): copilot 수정사항 반영

--- src/widgets/new-item-modal/types/index.ts
+++ src/widgets/new-item-modal/types/index.ts
@@ -3,7 +3,7 @@ import { Dayjs } from "dayjs";
 export type TimeZone = "오전" | "오후";
 
 export interface TimeSelection {
-  hour: number;
+  hour: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
   minute: number;
   timezone: TimeZone;
 }
