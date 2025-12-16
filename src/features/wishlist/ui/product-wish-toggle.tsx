0834b11866af7e5e8ffacc55ba11bfb22c61c88d
SeHyun <rla6170@naver.com>
2025-12-16 11:33:40 +0900

    feat(auction-detail): 상품 좋아요 토글 버튼 추가

---
+++ src/features/wishlist/ui/product-wish-toggle.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useState } from "react";
+
+import { Heart } from "lucide-react";
+
+import { cn } from "@/shared/lib/utils/utils";
+import Button from "@/shared/ui/button/button";
+
+export default function ProductWishToggle() {
+  const [isActive, setIsActive] = useState(false);
+  return (
+    <Button
+      variant="ghost"
+      className={cn(
+        "h-auto w-12 flex-col gap-2 py-2 text-xs",
+        isActive ? "text-muted-foreground" : "text-primary"
+      )}
+      size="sm"
+      onClick={() => setIsActive((prev) => !prev)}
+    >
+      <Heart
+        className={cn(isActive ? "text-zinc-800 dark:text-zinc-300" : "fill-red-500 text-red-500")}
+      />
+      1.1
+    </Button>
+  );
+}
