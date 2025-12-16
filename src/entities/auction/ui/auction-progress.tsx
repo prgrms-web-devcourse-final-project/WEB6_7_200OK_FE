e1cb03d2075fbdc6533d0b15983f5dc9b1a0e501
SeHyun <rla6170@naver.com>
2025-12-16 15:51:47 +0900

    chore(auction-detail): 시간 표현 태그 변경 span -> time

--- src/entities/auction/ui/auction-progress.tsx
+++ src/entities/auction/ui/auction-progress.tsx
@@ -7,7 +7,7 @@ export default function AuctionProgress() {
     <div className="flex flex-col gap-1">
       <div className="flex items-center justify-between">
         <h3 className="text-lg font-normal">다음 가격 하락까지</h3>
-        <span className="text-2xl font-medium">4:46</span>
+        <time className="text-2xl font-medium">4:46</time>
       </div>
       <Progress value={30} indicatorClassName="bg-brand" />
     </div>
