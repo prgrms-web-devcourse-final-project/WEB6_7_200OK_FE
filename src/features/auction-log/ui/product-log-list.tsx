ca8cc01d2dfdffb0759c40795b1e4b8a74261683
SeHyun <rla6170@naver.com>
2025-12-16 15:19:39 +0900

    feat(auction-detail): 경매 상세 페이지 로그 리스트 컴포넌트 추가

---
+++ src/features/auction-log/ui/product-log-list.tsx
@@ -0,0 +1,59 @@
+import { cn } from "@/shared/lib/utils/utils";
+
+export default function ProductLogList({ isSheet = false }: { isSheet?: boolean }) {
+  const MOCK = [
+    {
+      prev: "76800",
+      now: "75000",
+      time: "50분전",
+    },
+    {
+      prev: "76800",
+      now: "74000",
+      time: "50분전",
+    },
+    {
+      prev: "76800",
+      now: "73000",
+      time: "50분전",
+    },
+    {
+      prev: "76800",
+      now: "72000",
+      time: "50분전",
+    },
+    {
+      prev: "76800",
+      now: "71000",
+      time: "50분전",
+    },
+  ];
+  return (
+    <div className="flex flex-col gap-3">
+      <div className="flex items-center gap-3">
+        <h3 className={cn(isSheet ? "text-base" : "text-2xl", "font-medium")}>
+          하락 {MOCK.length}
+        </h3>
+        <span className="text-base text-red-400">▼ 40,000(-5.0%)</span>
+      </div>
+      <ul className={cn("flex flex-col", isSheet ? "gap-3" : "gap-1")}>
+        {MOCK.map((item) => (
+          <ProductLogListItem key={item.now} prev={item.prev} now={item.now} time={item.time} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function ProductLogListItem({ prev, now, time }: { prev: string; now: string; time: string }) {
+  return (
+    <li className="flex justify-between text-base">
+      <div className="flex gap-1 font-medium">
+        <span className="text-red-400 line-through">{prev}</span>
+        <span>→</span>
+        <span>{now}</span>
+      </div>
+      <span className="text-muted-foreground">{time}</span>
+    </li>
+  );
+}
