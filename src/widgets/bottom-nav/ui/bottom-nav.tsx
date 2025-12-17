4b07125d0241cb6ac522fab8f4d01e634b929bc0
Sage <prestoferoce@gmail.com>
2025-12-17 22:01:04 +0900

    refactor(bottom-nav): BottomNavId 타입 import 수정
    - BottomNav 컴포넌트에서 BottomNavId import를 BottomNavIdType으로 변경

--- src/widgets/bottom-nav/ui/bottom-nav.tsx
+++ src/widgets/bottom-nav/ui/bottom-nav.tsx
@@ -2,14 +2,14 @@
 
 import { usePathname } from "next/navigation";
 
-import { BOTTOM_NAV_ITEMS, BottomNavId } from "@/widgets/bottom-nav/model/item";
+import { BOTTOM_NAV_ITEMS, BottomNavIdType } from "@/widgets/bottom-nav/model/item";
 import { BottomNavItem } from "@/widgets/bottom-nav/ui/bottom-nav-item";
 
 export function BottomNav() {
   const pathname = usePathname();
 
   // TODO: 알림 연결 후 하드 코딩 삭제
-  const hasNotificationsById: Partial<Record<BottomNavId, boolean>> = {
+  const hasNotificationsById: Partial<Record<BottomNavIdType, boolean>> = {
     notification: true,
   };
 
