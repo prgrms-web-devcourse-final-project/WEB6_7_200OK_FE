2f44b11960e7a2612f75444564ce8e57cf664880
Sage <prestoferoce@gmail.com>
2025-12-17 09:22:04 +0900

    feat(date): dayjs 유틸리티 추가 및 한국 시간대 설정
    - 한국어 로케일을 적용한 dayjs 유틸리티 모듈 추가
    - UTC 및 timezone 플러그인을 설정하여 일관된 날짜 처리 지원

---
+++ src/shared/lib/utils/dayjs.ts
@@ -0,0 +1,10 @@
+import dayjs from "dayjs";
+import "dayjs/locale/ko";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.locale("ko");
+
+export { dayjs };
