635a75776b914361f53d579e6df283aab03bb1cb
SeHyun <rla6170@naver.com>
2025-12-17 15:16:58 +0900

    feat(auction): Auction 타입 추가

---
+++ src/entities/auction/model/types.ts
@@ -0,0 +1,25 @@
+export interface AuctionPriceType {
+  startPrice: number;
+  currentPrice: number;
+  discountRate: number;
+  stopLoss?: number;
+}
+
+export interface AuctionItemType {
+  auctionId: number;
+  imageUrl: string;
+  title: string;
+  isLiked: boolean;
+  startedAt: string;
+}
+
+export type AuctionStatusType = "SCHEDULED" | "PROCESS" | "COMPLETED" | "FAILED" | "CANCELED";
+/*
+SCHEDULED("경매 예정"),
+PROCESS("경매 진행 중"),
+COMPLETED("낙찰 완료"),
+FAILED("유찰"),
+CANCELED("경매 취소");
+*/
+
+export type AuctionType = AuctionItemType & AuctionPriceType;
