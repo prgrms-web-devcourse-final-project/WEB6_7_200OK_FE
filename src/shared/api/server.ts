344e9e79aa290bf909154c8a8142cf94f9b48951
SeHyun <rla6170@naver.com>
2025-12-18 17:41:05 +0900

    chore(api): env public 수정

--- src/shared/api/server.ts
+++ src/shared/api/server.ts
@@ -1,6 +1,6 @@
 import { cookies } from "next/headers";
 
-const API_URL = process.env.API_URL ?? "http://localhost:8080";
+const API_URL = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8080";
 
 interface StrictRequestInit<TRequest> extends Omit<RequestInit, "body"> {
   body?: TRequest;
