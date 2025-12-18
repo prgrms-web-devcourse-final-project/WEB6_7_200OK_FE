e0986126cf7e915105d491b095b516bf060a4316
SeHyun <rla6170@naver.com>
2025-12-18 17:33:22 +0900

    chore(api): api Error class 제거

--- src/shared/api/server.ts
+++ src/shared/api/server.ts
@@ -1,7 +1,5 @@
 import { cookies } from "next/headers";
 
-import { ApiError } from "@/shared/api/error/api-error";
-
 const API_URL = process.env.API_URL ?? "http://localhost:8080";
 
 interface StrictRequestInit<TRequest> extends Omit<RequestInit, "body"> {
@@ -28,18 +26,6 @@ export async function fetch<TResponse, TRequest = unknown>(
     body: init?.body ? JSON.stringify(init.body) : undefined,
     cache: "no-store",
   });
-
-  if (!res.ok) {
-    const errorData = await res.json().catch(() => ({}));
-
-    throw new ApiError({
-      message: errorData.message || "API 호출 중 오류가 발생했습니다.",
-      code: errorData.errorCode || "UNKNOWN_ERROR",
-      status: res.status,
-      displayMessage: errorData.userMessage,
-      data: errorData,
-    });
-  }
 
   return res.json() as Promise<TResponse>;
 }
