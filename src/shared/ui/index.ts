8f84cbc99cc877baf53dc0b30b13ad4d9db9ba60
SeHyun <rla6170@naver.com>
2025-12-18 09:45:59 +0900

    chore(ui): shared/ui 배럴 export 기반 index.ts 파일 추가

---
+++ src/shared/ui/index.ts
@@ -0,0 +1,93 @@
+export {
+  AlertDialog,
+  AlertDialogPortal,
+  AlertDialogOverlay,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogFooter,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogAction,
+  AlertDialogCancel,
+} from "./alert-dialog/alert-dialog";
+
+export { Avatar, AvatarImage, AvatarFallback } from "./avatar/avatar";
+
+export { default as Button, buttonVariants } from "./button/button";
+
+export { Calendar, CalendarDayButton } from "./calendar/calendar";
+
+export {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselPrevious,
+  CarouselNext,
+} from "./carousel/carousel";
+
+export {
+  type ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+  ChartLegend,
+  ChartLegendContent,
+  ChartStyle,
+} from "./chart/chart";
+
+export { default as EmptyState } from "./empty/empty";
+
+export { ErrorTemplate } from "./error/error-template";
+
+export { InfoAlert } from "./info-alert/info-alert";
+
+export { default as FileInput } from "./input/file-input";
+export { default as Input } from "./input/input";
+export { default as SearchInput } from "./input/search-input";
+
+export { DashboardListLayout } from "./layout/dashboard-list-layout";
+
+export { ConfirmDeleteModal } from "./modal/confirm-delete-modal";
+
+export { Progress } from "./progress/progress";
+
+export { type RatingButtonProps, type RatingProps, RatingButton, Rating } from "./rating/rating";
+
+export { ScrollArea, ScrollBar } from "./scroll-area/scroll-area";
+
+export {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectScrollDownButton,
+  SelectScrollUpButton,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from "./select/select";
+
+export { default as Separator } from "./separator/separator";
+
+export {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetOverlay,
+  SheetPortal,
+  SheetTitle,
+  SheetTrigger,
+} from "./sheet/sheet";
+
+export { Skeleton } from "./skeleton/skeleton";
+
+export { Switch } from "./switch/switch";
+
+export { Textarea } from "./textarea/textarea";
+
+export { ToastRegistry } from "./toast/toast-registry";
