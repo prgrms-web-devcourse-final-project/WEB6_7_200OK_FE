bb35a7959ccb478e5cebd825ad67e5c46dd21307
elecharu <elec_haru@naver.com>
2025-12-19 12:35:51 +0900

    refactor(auction): remove unused "use client" directives and delete Base64Image component

--- src/features/auction/auction-new/index.ts
+++
@@ -1,4 +0,0 @@
-export { PriceInputSection } from "./ui/price-input-section";
-export { TagInputSection } from "./ui/tag-input-section";
-export { useItemForm } from "./model/item-form";
-export { usePriceValidation } from "./model/price-validation";
