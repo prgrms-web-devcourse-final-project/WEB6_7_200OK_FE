1152c710c4516a2f2979e6a91a9445fde0066a3f
SeHyun <rla6170@naver.com>
2025-12-19 11:43:18 +0900

    chore(api): fetch util cache 커스텀 추가, 쿠키 join 오타  수정, localhost 제거

--- src/shared/api/server.ts
+++ src/shared/api/server.ts
@@ -2,7 +2,7 @@ import { cookies } from "next/headers";
 
 import type { ApiResponseType } from "@/shared/api/types/response";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8080";
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 interface StrictRequestInit<TRequest> extends Omit<RequestInit, "body"> {
   body?: TRequest;
@@ -16,7 +16,7 @@ export async function fetch<TResponse, TRequest = unknown>(
   const cookieHeader = cookieStore
     .getAll()
     .map((cookie) => `${cookie.name}=${cookie.value}`)
-    .join(";");
+    .join("; ");
 
   const res = await globalThis.fetch(`${API_URL}${path}`, {
     ...init,
@@ -26,7 +26,7 @@ export async function fetch<TResponse, TRequest = unknown>(
       ...(init?.headers || {}),
     },
     body: init?.body ? JSON.stringify(init.body) : undefined,
-    cache: "no-store",
+    cache: init?.cache ?? "no-store",
   });
 
   return res.json() as Promise<ApiResponseType<TResponse>>;
