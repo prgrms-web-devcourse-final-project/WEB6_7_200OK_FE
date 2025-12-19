61639985c15b866b1f320e60ae7c91da9147bcb9
elecharu <elec_haru@naver.com>
2025-12-19 09:24:43 +0900

    refactor(styles): 불필요한 cn 삭제

--- src/widgets/new-item-modal/ui/date-selector.tsx
+++ src/widgets/new-item-modal/ui/date-selector.tsx
@@ -6,7 +6,6 @@ import dayjs from "dayjs";
 import { Calendar as CalendarIcon } from "lucide-react";
 
 import type { DateSelectorProps } from "@/entities/date-modal";
-import { cn } from "@/shared/lib/utils/utils";
 import { Calendar } from "@/shared/ui/calendar/calendar";
 
 export function DateSelector({
@@ -63,10 +62,9 @@ export function DateSelector({
             month_caption: "hidden",
             today: "bg-transparent text-foreground",
             week: "flex w-full",
-            day: cn("relative flex-1 aspect-square p-0 flex items-center justify-center"),
-            day_button: cn(
-              "w-full h-full border-none focus:border-none focus:ring-0 focus:outline-none focus-visible:border-none focus-visible:ring-0 focus-visible:outline-none text-sm font-normal data-[selected-single=true]:bg-brand data-[selected-single=true]:text-accent dark:data-[selected-single=true]:text-accent-foreground data-[selected-single=true]:rounded-md data-[disabled=true]:text-accent data-[disabled=true]:opacity-50 data-[disabled=true]:cursor-not-allowed flex items-center justify-center"
-            ),
+            day: "relative flex-1 aspect-square p-0 flex items-center justify-center",
+            day_button:
+              "w-full h-full border-none focus:border-none focus:ring-0 focus:outline-none focus-visible:border-none focus-visible:ring-0 focus-visible:outline-none text-sm font-normal data-[selected-single=true]:bg-brand data-[selected-single=true]:text-accent dark:data-[selected-single=true]:text-accent-foreground data-[selected-single=true]:rounded-md data-[disabled=true]:text-accent data-[disabled=true]:opacity-50 data-[disabled=true]:cursor-not-allowed flex items-center justify-center",
           }}
           className="bg-accent w-full"
           showOutsideDays={false}
