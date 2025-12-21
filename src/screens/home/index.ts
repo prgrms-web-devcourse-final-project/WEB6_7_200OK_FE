60033cc97be48488309d3e1287a80acff6c801f0
Sage <prestoferoce@gmail.com>
2025-12-21 10:14:28 +0900

    refactor(home): HomeScreen 단일 파일 컴포넌트로 변경
    - HomeScreen과 HomeLayout을 하나의 HomeScreen 컴포넌트로 통합
    - index.tsx에서 HomeScreen 단일 export로 구조 변경
    - 메인 페이지에서 새로운 HomeScreen export 사용하도록 수정
    - 사용되지 않는 HomeLayout 관련 파일 제거

--- src/screens/home/index.ts
+++
@@ -1,2 +0,0 @@
-export { HomeScreen } from "./ui/HomeScreen";
-export { HomeLayout } from "./ui/HomeLayout";
--- src/screens/home/ui/HomeLayout.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactNode } from "react";
-
-interface HomeLayoutProps {
-  children: ReactNode;
-}
-
-export function HomeLayout({ children }: HomeLayoutProps) {
-  return <>{children}</>;
-}
