98ca2ed15cb7d78ff342a202cec9ff1fd2b5d00b
SeHyun <rla6170@naver.com>
2025-12-22 09:43:37 +0900

    chore(auction-detail): seller type username 타입 컨벤션 수정

--- src/features/auction/auction-sale/model/types.ts
+++ src/features/auction/auction-sale/model/types.ts
@@ -1,7 +1,7 @@
 export interface SellerType {
   // TODO: 추후 user profile type 수정
   sellerId: number;
-  nickname: string;
+  username: string;
   profileImageUrl?: string;
   rating: number;
   reviewCount: number;
