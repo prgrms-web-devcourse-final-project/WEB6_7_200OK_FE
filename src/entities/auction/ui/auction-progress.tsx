6e03e0c4b9cbde28a832b698b38ebe744ecae93a
SeHyun <rla6170@naver.com>
2025-12-23 11:22:16 +0900

    feat(auction-detail): auction ticker model 및 provider screen 및 progress 연결

--- src/entities/auction/ui/auction-progress.tsx
+++ src/entities/auction/ui/auction-progress.tsx
@@ -1,15 +1,22 @@
+"use client";
+
+import { useAuctionTicker } from "@/entities/auction/hooks/use-auction-ticker";
+import { calcMsPercent } from "@/shared/lib/utils/time/calc";
+import { formatMs } from "@/shared/lib/utils/time/format";
 import { Progress } from "@/shared/ui/progress/progress";
 
 export function AuctionProgress() {
-  // TODO: timeout end trigger
-  // TODO: timeout get default time
+  const { remainMs, duration } = useAuctionTicker();
+
+  const formatted = formatMs(remainMs);
+  const { remain } = calcMsPercent(remainMs, duration);
   return (
     <div className="flex flex-col gap-1">
       <div className="flex items-center justify-between">
         <h3 className="text-lg font-normal">다음 가격 하락까지</h3>
-        <time className="text-2xl font-medium">4:46</time>
+        <time className="text-2xl font-medium">{formatted}</time>
       </div>
-      <Progress value={30} indicatorClassName="bg-brand" />
+      <Progress value={remain} indicatorClassName="bg-brand" />
     </div>
   );
 }
