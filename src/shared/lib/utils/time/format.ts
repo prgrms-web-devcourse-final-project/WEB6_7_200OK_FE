ac53ead6bf6df0f5b3608df44c3376fdef92eeb7
SeHyun <rla6170@naver.com>
2025-12-23 11:16:29 +0900

    feat(auction-detail): ms 기반 포맷팅 및 계산 유틸 함수 추가

--- src/shared/lib/utils/time/format.ts
+++ src/shared/lib/utils/time/format.ts
@@ -16,3 +16,10 @@ export function formatAgo(input: string | Date | number): string {
   const diffDay = now.diff(t, "day");
   return `${diffDay}일 전`;
 }
+
+export function formatMs(ms: number) {
+  const totalSec = Math.ceil(ms / 1000);
+  const mm = Math.floor(totalSec / 60);
+  const ss = totalSec % 60;
+  return `${String(mm).padStart(2, "0")}:${String(ss).padStart(2, "0")}`;
+}
