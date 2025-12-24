ae65f073f25ad0d8bd974d0b5411b5001a3cbf3b
SeHyun <rla6170@naver.com>
2025-12-24 11:31:58 +0900

    feat(auction-detail): auction detail price store price 연결

--- src/widgets/auction/auction-detail/ui/auction-detail-price.tsx
+++ src/widgets/auction/auction-detail/ui/auction-detail-price.tsx
@@ -1,17 +1,20 @@
+"use client";
+
 import { formatPriceKRW } from "@/shared/lib/utils/price/formatPriceKRW";
+import { useAuctionPriceStore } from "@/widgets/auction/auction-detail/hooks/auction-price-store-provider";
 
 interface AuctionDetailPriceProps {
   startPrice: number;
-  currentPrice: number;
 }
 
-export default function AuctionDetailPrice({ startPrice, currentPrice }: AuctionDetailPriceProps) {
+export default function AuctionDetailPrice({ startPrice }: AuctionDetailPriceProps) {
+  const price = useAuctionPriceStore((state) => state.price);
   return (
     <div className="flex flex-col gap-2">
       <span className="text-muted-foreground text-base font-medium">
         판매 시작가 {formatPriceKRW(startPrice)}원
       </span>
-      <span className="text-4xl font-semibold">{formatPriceKRW(currentPrice)}원</span>
+      <span className="text-4xl font-semibold">{formatPriceKRW(price)}원</span>
     </div>
   );
 }
