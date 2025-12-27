969f161d50a26788e800419be543993fd0ec3bd7
Sage <prestoferoce@gmail.com>
2025-12-27 17:35:33 +0900

    refactor(main): HomeScreen을 MainScreen으로 교체 및 레이아웃 수정
    - HomeScreen을 제거하고 MainScreen으로 변경
    - page.tsx에서 MainScreen을 사용하도록 수정
    - 기존 home screen 관련 구현 파일 정리
    - layout.tsx 구조 및 스타일을 MainScreen 기준으로 조정
    - HeroSection 버튼을 Button 컴포넌트 대신 native button으로 변경

--- src/screens/home/index.tsx
+++
@@ -1,8 +0,0 @@
-import { getHomeAuctions } from "@/screens/home/api/getHomeActions";
-import HomeMain from "@/screens/home/ui/home-main";
-
-export default async function HomeScreen() {
-  const data = await getHomeAuctions();
-
-  return <HomeMain data={data} />;
-}
---
+++ src/screens/main/index.tsx
@@ -0,0 +1,10 @@
+import { HeroSection } from "@/widgets/hero-section";
+
+export default async function MainScreen() {
+  return (
+    <>
+      <HeroSection />
+      {/* <Auctions /> */}
+    </>
+  );
+}
