2fec92735f4bfec066e74da1c0a7e3a2be79eb3b
Sage <prestoferoce@gmail.com>
2025-12-28 13:32:53 +0900

    refactor(auction-carousel): AuctionCarouselSkeleton에서 CardSkeleton 사용
    - AuctionCarouselSkeleton의 인라인 skeleton 마크업 제거
    - auction-item-card의 CardSkeleton 컴포넌트를 사용하도록 변경

--- src/widgets/auction/auction-carousel-section/ui/auction-carousel-skeleton.tsx
+++ src/widgets/auction/auction-carousel-section/ui/auction-carousel-skeleton.tsx
@@ -1,4 +1,4 @@
-import { Skeleton } from "@/shared/ui";
+import { CardSkeleton } from "@/entities/auction/ui/auction-item-card/ui/card-skeleton";
 
 export default function AuctionCarouselSkeleton() {
   return (
@@ -9,14 +9,7 @@ export default function AuctionCarouselSkeleton() {
           key={index}
           className="shrink-0 basis-[70%] sm:basis-1/2 md:basis-2/5 lg:basis-1/3 xl:basis-1/4"
         >
-          <div className="flex flex-col gap-3">
-            <Skeleton className="aspect-square w-full rounded-xl" />
-            <div className="flex flex-col gap-2">
-              <Skeleton className="h-4 w-3/4" />
-              <Skeleton className="h-4 w-1/2" />
-              <Skeleton className="h-3 w-1/3" />
-            </div>
-          </div>
+          <CardSkeleton />
         </div>
       ))}
     </div>
