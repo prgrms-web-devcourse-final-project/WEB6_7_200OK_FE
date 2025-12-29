73aa14b4df37f5788a0ff526d29100280dc81930
Sage <prestoferoce@gmail.com>
2025-12-29 20:17:44 +0900

    feat(time): auctions client와 server time store 동기화
    - AuctionsData에 serverAt 필드 추가
    - AuctionsClient에서 useServerTimeStore를 사용해 서버 시간 동기화
    - server-time-store에 마지막 동기화 시점 추적 로직 추가
    - 중복 서버 시간 업데이트를 방지하도록 조건 처리
    - getCurrentServerTime 반환 값을 timestamp 형태로 변경

--- src/screens/main/model/types.ts
+++ src/screens/main/model/types.ts
@@ -1,6 +1,7 @@
 import type { AuctionType } from "@/entities/auction/model/types";
 
 export interface AuctionsData {
+  serverAt: string;
   popularList: AuctionType[];
   processList: AuctionType[];
   scheduledList: AuctionType[];
