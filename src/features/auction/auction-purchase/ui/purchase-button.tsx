70fd1c5a1e7bd8b4f1db78c8d60d4b373962981b
SeHyun <rla6170@naver.com>
2025-12-30 13:56:22 +0900

    feat(toss-payments): toss payments widget 화면 연결 및 purchase button 분리

--- src/features/auction/auction-purchase/ui/purchase-button.tsx
+++ src/features/auction/auction-purchase/ui/purchase-button.tsx
@@ -2,9 +2,13 @@
 
 import { Button } from "@/shared/ui";
 
-export default function PurchaseButton() {
+export default function PurchaseButton({
+  onClick,
+}: {
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+}) {
   return (
-    <Button size="lg" className="h-14 flex-1">
+    <Button size="lg" className="h-14 flex-1" onClick={onClick}>
       구매하기
     </Button>
   );
