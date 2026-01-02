106d388816a23239595dbd891f58938653140d73
Sage <prestoferoce@gmail.com>
2026-01-02 15:59:06 +0900

    refactor(auction-list): 경매 목록 화면 및 UI 구조 정리
    - AuctionListHeader, AuctionListToolbar, AuctionList를 사용하는 구조로 화면 재구성
    - 임시로 사용하던 dynamic export 제거
    - 사용되지 않는 props 정리
    - AuctionList placeholder 컴포넌트 추가
    - AuctionGrid props 구조 정리

--- src/app/(public)/auctions/page.tsx
+++ src/app/(public)/auctions/page.tsx
@@ -1,8 +1,5 @@
 import AuctionListScreen from "@/screens/auction/auction-list";
 
-// FIXME: 임시 처리
-export const dynamic = "force-dynamic";
-
 export default function Page() {
   return <AuctionListScreen />;
 }
