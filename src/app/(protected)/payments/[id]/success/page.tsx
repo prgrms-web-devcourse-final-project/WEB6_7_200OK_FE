5436079e90622c9f47ee9d00449d184470d1fda3
SeHyun <rla6170@naver.com>
2026-01-02 21:31:08 +0900

    chore(auction-purchase): payments success fail routing (protect) 라우팅 그룹으로 이동

--- src/app/(protected)/payments/[id]/success/page.tsx
+++ src/app/(protected)/payments/[id]/success/page.tsx
@@ -1,5 +1,5 @@
 import AuctionPurchaseSuccessScreen from "@/screens/auction/auction-purchase/ui/AuctionPurchaseSuccessScreen";
 
-export default function Page() {
+export default function page() {
   return <AuctionPurchaseSuccessScreen />;
 }
