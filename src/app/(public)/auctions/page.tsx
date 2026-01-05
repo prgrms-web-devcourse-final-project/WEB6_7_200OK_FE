c73b8b478b51adb5623dfb5a62bc22ba5f82d3cd
Sage <prestoferoce@gmail.com>
2026-01-05 12:40:21 +0900

    refactor(filters): parseFilters 파일 위치 변경

--- src/app/(public)/auctions/page.tsx
+++ src/app/(public)/auctions/page.tsx
@@ -1,7 +1,7 @@
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
+import { parseFilters } from "@/features/auction/filters/model/parse-filters";
 import AuctionListScreen from "@/screens/auction/auction-list";
-import { parseFilters } from "@/screens/auction/auction-list/model/parse-filters";
 import { searchAuctionsQuery } from "@/screens/auction/auction-list/model/search-auctions-query";
 import type { SearchParamsType } from "@/screens/auction/auction-list/model/types";
 import { getQueryClient } from "@/shared/api/query-client";
