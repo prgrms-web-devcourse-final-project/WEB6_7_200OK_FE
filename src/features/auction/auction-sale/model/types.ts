a77a384411589ce0753895e69558921520d96847
SeHyun <rla6170@naver.com>
2026-01-05 12:18:12 +0900

    feat(auction-detail): 판매자 리뷰, 판매 품목 api 수정 사항 반영

--- src/features/auction/auction-sale/model/types.ts
+++ src/features/auction/auction-sale/model/types.ts
@@ -1,3 +1,5 @@
+import { AuctionType } from "@/entities/auction/model/types";
+
 export interface SellerType {
   // TODO: 추후 user profile type 수정
   sellerId: number;
@@ -5,4 +7,22 @@ export interface SellerType {
   profileImageUrl?: string;
   rating: number;
   reviewCount: number;
+}
+
+export interface BuyersType {
+  buyerId: number;
+  username: string;
+  content: string;
+}
+
+export type SellerAuctionType = Pick<AuctionType, "auctionId" | "title"> & {
+  auctionImageUrl: string;
+};
+export interface AuctionSellerInfoType {
+  sellerId: number;
+  username: string;
+  rating: number;
+  totalReviews: number;
+  buyers: BuyersType[];
+  auctions: SellerAuctionType[];
 }
