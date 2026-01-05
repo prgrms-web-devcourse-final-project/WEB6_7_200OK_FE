95c40dfd165b1ca083198c14b6bc58073d2ac0e2
Sage <prestoferoce@gmail.com>
2026-01-05 12:42:03 +0900

    refactor(filters): 필터 타입 이동 및 정리
    - 경매 필터 관련 타입/상수 정의를 src/features/auction/filters/model/types.ts로 이동 및 통합
    - auction-list model에서 중앙화된 types를 사용하도록 참조 경로 수정
    - auction-filters widget의 중복 types 파일 제거

--- src/widgets/auction/auction-filters/model/types.ts
+++
@@ -1,9 +0,0 @@
-import { CategoryFilter } from "@/entities/auction/model/category";
-import { AuctionStatusFilterValueType } from "@/entities/auction/model/status";
-import { PriceFilterValue } from "@/widgets/auction/auction-filters/model/constants";
-
-export interface AuctionFiltersType {
-  price: PriceFilterValue;
-  status: AuctionStatusFilterValueType;
-  category: CategoryFilter;
-}
