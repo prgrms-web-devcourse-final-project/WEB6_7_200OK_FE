3233a960bada8d815a8a1b6d576f10fc4c323a19
Sage <prestoferoce@gmail.com>
2026-01-06 20:11:26 +0900

    feat(endpoint):  Auction Notification Start API 추가

--- src/shared/config/endpoints.ts
+++ src/shared/config/endpoints.ts
@@ -9,6 +9,8 @@ export const API_ENDPOINTS = {
   auctionSellerInfo: (sellerId: number | string) => `/api/v1/auctions/${sellerId}/seller`,
   auctionNotificationSetting: (auctionId: string | number) =>
     `/api/v1/auctions/${auctionId}/notification-settings`,
+  auctionNotificationSettingStart: (auctionId: string | number) =>
+    `/api/v1/auctions/${auctionId}/notification-settings/start`,
   auctionRecentView: (auctionId: number | string) => `/api/v1/recentview/${auctionId}`,
 
   userInfo: (userid: number) => `/api/v1/users/${userid}`,
