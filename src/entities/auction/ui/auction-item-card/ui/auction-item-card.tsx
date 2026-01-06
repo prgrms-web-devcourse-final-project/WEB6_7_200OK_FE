b4856b147ab4f3e606fed357aa9b66ee61ee5cfc
Sage <prestoferoce@gmail.com>
2026-01-06 20:47:30 +0900

    feat(notify): NotifyCtaButton 추가

--- src/entities/auction/ui/auction-item-card/ui/auction-item-card.tsx
+++ src/entities/auction/ui/auction-item-card/ui/auction-item-card.tsx
@@ -12,9 +12,9 @@ import AuctionEndTimer from "@/entities/auction/ui/auction-item-card/ui/auction-
 import AuctionPrice from "@/entities/auction/ui/auction-item-card/ui/auction-price";
 import AuctionTimer from "@/entities/auction/ui/auction-item-card/ui/auction-timer";
 import BuyCtaButton from "@/entities/auction/ui/auction-item-card/ui/buy-cta-button";
-import NotifyCtaButton from "@/entities/auction/ui/auction-item-card/ui/cta-button";
 import EndedCtaButton from "@/entities/auction/ui/auction-item-card/ui/ended-cta-button";
 import { LikeButton } from "@/entities/auction/ui/auction-item-card/ui/like-button";
+import NotifyCtaButton from "@/entities/auction/ui/auction-item-card/ui/notify-cta-button";
 import RankingBadge from "@/entities/auction/ui/auction-item-card/ui/ranking-badge";
 import UpcomingInfo from "@/entities/auction/ui/auction-item-card/ui/upcoming-info";
 import { ROUTES } from "@/shared/config/routes";
