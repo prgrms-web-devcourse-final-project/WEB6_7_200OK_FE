f21eafec147433c37603a895b0c9202991709e28
Sage <prestoferoce@gmail.com>
2026-01-06 11:52:44 +0900

    refactor(layout): ReactNode import  수정

--- src/app/(public)/(category)/layout.tsx
+++ src/app/(public)/(category)/layout.tsx
@@ -1,8 +1,8 @@
-import { Suspense } from "react";
+import { type ReactNode, Suspense } from "react";
 
 import { CategoryNav } from "@/widgets/category-nav";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
       <Suspense fallback={null}>
