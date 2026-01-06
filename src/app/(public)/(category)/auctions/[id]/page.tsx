31cbc4ec54ff5fbbdc0b4a5fda71c4d8c7072811
SeHyun <rla6170@naver.com>
2026-01-06 11:35:29 +0900

    feat(auction-sale): 판매자 전용 이모지 웹 소켓 연결

--- src/app/(public)/(category)/auctions/[id]/page.tsx
+++ src/app/(public)/(category)/auctions/[id]/page.tsx
@@ -1,8 +1,13 @@
+import { cookies } from "next/headers";
+
 import { AuctionDetailScreen } from "@/screens/auction/auction-detail";
 import { auctionDetailLoader } from "@/screens/auction/auction-detail/api/loader";
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
   const { data } = await auctionDetailLoader(id);
-  return <AuctionDetailScreen data={data} id={id} />;
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get("accessToken")?.value;
+
+  return <AuctionDetailScreen data={data} id={id} token={accessToken} />;
 }
