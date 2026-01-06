a7934d68b92ba99fa7b9c7692b7b8cfa1e88caeb
SeHyun <rla6170@naver.com>
2026-01-06 22:30:08 +0900

    feat(auction-log): 로그 아이템 반영

--- src/features/auction/auction-log/ui/auction-log-chart.tsx
+++ src/features/auction/auction-log/ui/auction-log-chart.tsx
@@ -2,19 +2,8 @@
 
 import { Area, AreaChart, CartesianGrid, XAxis } from "recharts";
 
+import type { RecentPriceHistoryType } from "@/features/auction/auction-log/model/types";
 import { ChartContainer, ChartTooltip, ChartTooltipContent, type ChartConfig } from "@/shared/ui";
-
-const chartData = [
-  { price: "120000", watch: 186 },
-  { price: "130000", watch: 305 },
-  { price: "140000", watch: 237 },
-  { price: "150000", watch: 73 },
-  { price: "160000", watch: 209 },
-  { price: "170000", watch: 214 },
-  { price: "180000", watch: 73 },
-  { price: "190000", watch: 209 },
-  { price: "200000", watch: 214 },
-];
 
 const chartConfig = {
   watch: {
@@ -23,30 +12,24 @@ const chartConfig = {
   },
 } satisfies ChartConfig;
 
-export default function AuctionLogChart() {
+export default function AuctionLogChart({ item }: { item: RecentPriceHistoryType[] }) {
   return (
     <div className="flex flex-col gap-3">
       <h3 className="text-base font-medium">관심도</h3>
       <ChartContainer config={chartConfig}>
         <AreaChart
           accessibilityLayer
-          data={chartData}
+          data={item}
           margin={{
             left: 0,
             right: 0,
           }}
         >
           <CartesianGrid vertical={false} />
-          <XAxis
-            dataKey="price"
-            tickLine={false}
-            axisLine={false}
-            tickMargin={8}
-            tickFormatter={(value) => value.slice(0, 3)}
-          />
+          <XAxis dataKey="currentPrice" tickLine={false} axisLine={false} tickMargin={8} />
           <ChartTooltip cursor={false} content={<ChartTooltipContent indicator="line" />} />
           <Area
-            dataKey="watch"
+            dataKey="viewerCount"
             type="natural"
             fill="var(--color-brand)"
             fillOpacity={0.4}
