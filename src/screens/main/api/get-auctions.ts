cb241ad105c40dc3c5afc415dd2c140f3c7198d5
Sage <prestoferoce@gmail.com>
2026-01-07 06:59:09 +0900

    feat(api): getAuctionsClient API 추가

--- src/screens/main/api/get-auctions.ts
+++ src/screens/main/api/get-auctions.ts
@@ -1,9 +1,16 @@
 import { AuctionsData } from "@/screens/main/model/types";
+import { httpClient } from "@/shared/api/client";
 import { fetch } from "@/shared/api/server";
 import { API_ENDPOINTS } from "@/shared/config/endpoints";
 
 export const getAuctions = async () => {
   const response = await fetch<AuctionsData>(API_ENDPOINTS.auctions);
+
+  return response.data;
+};
+
+export const getAuctionsClient = async () => {
+  const response = await httpClient<AuctionsData>(API_ENDPOINTS.auctions);
 
   return response.data;
 };
